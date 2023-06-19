bd158b1a4045787282162a04744602939cbc70fb
Debashis Biswas <info@intolap.com>
2023-06-19 16:36:26 +0530

    removed build folder removed warnings

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import TriggerclickEvent from './examples/TriggerClickEvent';
 import Html2Canvas from './examples/Html2Canvas';
 import SetFocus from './examples/SetFocus';
 import Facebook from './examples/Facebook';
-import PlaceFinder from './examples/PlaceFinder';
-import PlaceFinder2 from './examples/PlaceFinder2';
-import SearchLocationInput from './examples/SearchLocationInput';
+// import PlaceFinder from './examples/PlaceFinder';
+// import PlaceFinder2 from './examples/PlaceFinder2';
+// import SearchLocationInput from './examples/SearchLocationInput';
 
 function App() {
   return (
@@ -26,9 +26,9 @@ function App() {
           <Route path="/html2canvas/" element={<Html2Canvas />}></Route>
           <Route path="/setfocus/" element={<SetFocus />}></Route>
           <Route path="/facebook/" element={<Facebook />}></Route>
-          <Route path="/placefinder/" element={<PlaceFinder />}></Route>
+          {/* <Route path="/placefinder/" element={<PlaceFinder />}></Route>
           <Route path="/placefinder2/" element={<PlaceFinder2 />}></Route>
-          <Route path="/SearchLocationInput/" element={<SearchLocationInput />}></Route>
+          <Route path="/SearchLocationInput/" element={<SearchLocationInput />}></Route> */}
         </Routes>
       </Suspense>
       {/* <Footer/> */}
